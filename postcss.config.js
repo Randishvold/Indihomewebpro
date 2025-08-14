4fd1e1725a330922b982dd83a391092474306d27
Randishvold <randish.dev@gmail.com>
2025-08-15 00:26:38 +0700

    update

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,12 @@
 import autoprefixer from 'autoprefixer';
-import purgecss from '@fullhuman/postcss-purgecss';
+import purgecssPlugin from '@fullhuman/postcss-purgecss';
 
 const config = {
   plugins: [
     autoprefixer(),
     ...(process.env.NODE_ENV === 'production'
       ? [
-          purgecss({
+          purgecssPlugin.default({
             content: ['./src/**/*.html', './src/**/*.svelte'],
             defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
           }),
